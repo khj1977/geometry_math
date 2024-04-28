de9b5015c78b4e6842b7432d26420beee492782f
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-04-29 08:30:35 +0900

    glut sample for mouse event and small animation has been under dev. There are still bug and it would be fixed later on.

--- play-ground/glut-test.py
+++ play-ground/glut-test.py
@@ -5,6 +5,26 @@ from OpenGL.GLUT import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 
+import math as m
+
+# dirty but test.
+theta = 0.0
+deltaTheta = 0.01
+xx = 0.0
+yy = 0.0
+
+def idle():
+    glutPostRedisplay()
+
+def mouse(button, state, x, y):
+  if button == GLUT_LEFT_BUTTON:
+    if (state == GLUT_DOWN):
+      glutIdleFunc(idle)
+    else:
+      glutIdleFunc(0)
+  elif button == GLUT_RIGHT_BUTTON:
+    if state == GLUT_DOWN:
+        glutPostRedisplay()
 
 def display():
     glClear(GL_COLOR_BUFFER_BIT)
@@ -16,6 +36,26 @@ def display():
     glEnd()
     glFlush()
 
+def displayCircle():
+    # static theta = 0.0
+    # static deltaTheta = 0.01
+    # static xx = 0.0
+    # static yy = 0.0
+
+    glClear(GL_COLOR_BUFFER_BIT)
+    glBegin(GL_POINTS)
+
+    glVertex2d(xx, yy)
+
+    glEnd()
+    glFlush()
+
+    xx = m.cos(theta)
+    yy = m.sin(theta)
+
+    theta = theta + deltaTheta
+    if (theta > 2.0 * 3.14):
+        theta = 0.0
 
 def init():
     glClearColor(0.0, 0.0, 0.0, 0.0)
@@ -29,6 +69,7 @@ glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
 glutInitWindowSize(300, 300)
 glutInitWindowPosition(200, 200)
 glutCreateWindow(b"hello")
+glutMouseFunc(mouse)
 init()
 glutDisplayFunc(display)
 glutMainLoop()
