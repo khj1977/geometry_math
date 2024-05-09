9728bcbff9017f037d271c496ab01f331d6d080d
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-05-09 10:50:50 +0900

    Skelton of simple ParticleManager has been added.

---
+++ package/com_gmail_eulerbonjour/particle/ParticleManager.py
@@ -0,0 +1,13 @@
+class ParticleManager:
+    
+    def __init__(self):
+        pass
+
+    def addParticle(self, aParticle):
+        pass
+
+    def getParticles(self):
+        pass
+
+    def getParticle(self, id):
+        pass
