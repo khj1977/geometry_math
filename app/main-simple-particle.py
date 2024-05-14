d99fb0a45dff417a6660949939e1983b26c90210
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-05-14 17:19:49 +0900

    minor fix.

--- app/main-simple-particle.py
+++ app/main-simple-particle.py
@@ -1,5 +1,4 @@
 from com_gmail_eulerbonjour.vector_field import ParticleWorld as w
-from com_gmail_eulerbonjour.vector_field import SimpleParticleManager as m
 
 # can this world be sharted with w.world?
 world = w.world
