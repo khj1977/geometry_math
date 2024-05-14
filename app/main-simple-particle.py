61d78443ad18d6cfd921f26c90b83fe9c26f6237
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-05-14 17:01:19 +0900

    main and remaining part of particle world has been impled.

---
+++ app/main-simple-particle.py
@@ -0,0 +1,6 @@
+from com_gmail_eulerbonjour.vector_field import ParticleWorld as w
+from com_gmail_eulerbonjour.vector_field import SimpleParticleManager as m
+
+# can this world be sharted with w.world?
+world = w.ParticleWorld()
+world.runGlut()
--- package/com_gmail_eulerbonjour/vector_field/ParticleWorld.py
+++ package/com_gmail_eulerbonjour/vector_field/ParticleWorld.py
@@ -7,6 +7,45 @@ from com_gmail_eulerbonjour.ode_solver import ode_time as time
 from com_gmail_eulerbonjour.vector_field import SimpleParticleManager as manager
 from com_gmail_eulerbonjour.vector_field import SimpleParticle as particle
 
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+import math as m
+
+def my_glut_idle():
+    glutPostRedisplay()
+
+def my_glut_mouse(button, state, x, y):
+  if button == GLUT_LEFT_BUTTON:
+    if (state == GLUT_DOWN):
+      glutIdleFunc(idle)
+    else:
+      glutIdleFunc(0)
+  elif button == GLUT_RIGHT_BUTTON:
+    if state == GLUT_DOWN:
+        glutPostRedisplay()
+
+def my_glut_init():
+    glClearColor(1.0, 1.0, 1.0, 0.0)
+    glMatrixMode(GL_PROJECTION)
+    
+
+    glLoadIdentity()
+    # glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0)
+    glOrtho(-2.0, 2.0, -2.0, 2.0, -10.0, 10.0)
+
+    gluPerspective(30.0, 1.0, 1.0, 100.0)
+    # glTranslated(0.0, 0.0, -5.0);
+    gluLookAt(0.0, 0.0, -10.0, 0.0, 0.0, 10.0, 0.0, 1.0, 0.0)
+
+def my_glut_display():
+    # dirty hack
+    global world
+
+    world.inc()
+    world.render()
+
 class ParticleWorld:
 
     def __init__(self):
@@ -50,3 +89,17 @@ class ParticleWorld:
         self.manager.incAll()
 
         return self
+    
+    def render(self):
+       self.manager.renderAll()
+
+    def runGlut(self):
+       glutInit(sys.argv)
+       glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB)
+       glutInitWindowSize(600, 600)
+       glutInitWindowPosition(200, 200)
+       glutCreateWindow(b"Particle World")
+       glutMouseFunc(my_glut_mouse)
+       my_glut_init()
+       glutDisplayFunc(my_glut_display)
+       glutMainLoop()
