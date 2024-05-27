7bc616a8070696f0ca997cb8c953d414c1b57175
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-05-27 13:17:08 +0900

    a raw sketch of simple mesh to manage relative positions of particles has been made.

---
+++ package/com_gmail_eulerbonjour/vector_field/SimpleMesh.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+class SimpleMesh:
+
+    def __init__(self, rows, cols, minX, minY, maxX, maxY):
+        self.internalMatrix = np.zeros((rows, cols))
+
+        self.rows = rows
+        self.cols = cols
+
+        self.minX = minX
+        self.maxX = maxX
+        self.minY = minY
+        self.maxY = maxY
+
+        self.calcScale()
+
+    def addParticle(self, row, col, particle):
+        self.internalMatrix[row][col] = particle
+
+        return self
+    
+    def addParticleByGlutPosition(self, x, y, particle):
+        row = self.xScale * x
+        col = self.yScale * y
+
+        self.internalMatrix[row][col] = particle
+
+        return self
+    
+    def getParticle(self, row, col):
+        return self.internalMatrix[row][col]
+    
+    def getParticleByGlutPosition(self, x, y):
+        row = self.xScale * x
+        col = self.yScale * y
+
+        return self.getParticle(row, col)
+
+    def calcScale(self):
+        self.xScale = self.rows / (self.maxX - self.minX)
+        self.yScale = self.cols / (self.maxY - self.minY)
+
+        return self
+
+
--- play-ground/np-test.py
+++ play-ground/np-test.py
@@ -3,3 +3,13 @@ import numpy as np
 v = np.array([2, 3])
 print(v[0])
 print(v[1])
+
+rows = 2
+cols = 3
+m = np.zeros((rows, cols))
+
+m[0][0] = 2.0
+m[1][0] = 3.0
+m[0][1] = 5.0
+
+print(m)
