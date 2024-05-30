c2ac41dca408e2e25fb7e25ceb046df1220a9360
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-05-30 23:23:12 +0900

    sample code for audio and mic for python has been added. It would be used to test of digital filter by z-transform later on.Comparison of filter by DFT and Z-Transform may have interest.

---
+++ play-ground/pyaudio-test.py
@@ -0,0 +1,63 @@
+# sample code from https://qiita.com/KENTAROSZK/items/8d82a495b7cffec69862 to understand pyaudio.
+
+from datetime import datetime
+import wave
+import time
+
+import pyaudio
+
+
+FORMAT        = pyaudio.paInt16
+TIME          = 10           # 録音時間[s]
+SAMPLE_RATE   = 44100        # サンプリングレート
+FRAME_SIZE    = 1024         # フレームサイズ
+CHANNELS      = 1            # モノラルかバイラルか
+INPUT_DEVICE_INDEX = 0       # マイクのチャンネル
+NUM_OF_LOOP   = int(SAMPLE_RATE / FRAME_SIZE * TIME)
+
+WAV_FILE = "./output.wav"
+
+pa = pyaudio.PyAudio()
+for i in range(pa.get_device_count()):
+    print(pa.get_device_info_by_index(i))
+    print()
+pa.terminate()
+
+def record_and_save():
+    """
+    デバイスから出力される音声の録音をする
+    """
+    pa = pyaudio.PyAudio()
+
+    stream = pa.open(format   = FORMAT,
+                     channels = CHANNELS,
+                     rate     = SAMPLE_RATE,
+                     input    = True,
+                     input_device_index = INPUT_DEVICE_INDEX,
+                     frames_per_buffer  = FRAME_SIZE)
+
+    print("RECORDING...")
+
+    list_frame = []
+
+
+    for i in range(NUM_OF_LOOP):
+        data = stream.read(FRAME_SIZE)
+        list_frame.append(data)
+
+    print("RECORDING DONE!")
+
+    # close and terminate stream object "stream"
+    stream.stop_stream()
+    stream.close()
+    pa.terminate()
+
+
+    wf = wave.open(WAV_FILE, 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(pa.get_sample_size(FORMAT))
+    wf.setframerate(SAMPLE_RATE)
+    wf.writeframes(b''.join(list_frame))
+    wf.close()
+
+record_and_save()
