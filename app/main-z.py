4b396b7816437eb3ad6404c586a93c07f5998f05
Hwi Jun KIM <euler.bonjour@gmail.com>
2024-06-03 11:59:45 +0900

    r has been incremented and 3-D plot of z-transform. Only positive region of n is calced and it seems fine.

--- app/main-z.py
+++ app/main-z.py
@@ -6,8 +6,10 @@ wt = 0.0
 
 deltaWt = 0.01
 
-nBegin = -1000.0
-nEnd = 1000.0
+# nBegin = -1000.0
+# nBegin = -100.0
+nBegin = 0.01
+nEnd = 100.0
 
 theta = 0.0
 deltaTheta = 0.01
@@ -25,14 +27,20 @@ while True:
     i = i + 1
 
 while True:
-    if (wt > 6.28):
+    if (r > 3.0):
         break
+    while wt < 6.28:
+        if (wt > 6.28):
+            break
 
-    xz = z.calcZTransOverN(xn, r, wt, nBegin, nEnd)
-    power = z.calcPowerOfZ(xz)
+        xz = z.calcZTransOverN(xn, r, wt, nBegin, nEnd)
+        power = z.calcPowerOfZ(xz)
 
-    # print(str(r) + "," + str(wt) + "," + str(xz[0]) + "," + str(xz[1]))
-    # print(str(r) + "," + str(wt) + "," + str(power))
-    print(power)
+        # print(str(r) + "," + str(wt) + "," + str(xz[0]) + "," + str(xz[1]))
+        print(str(r) + "," + str(wt) + "," + str(power))
+        # print(power)
+        # print(str(wt) + "," + str(power))
 
-    wt = wt + deltaWt
+        wt = wt + deltaWt
+    r = r + 0.1
+    wt = 0.0
